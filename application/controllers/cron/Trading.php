982ade813f69354a3ecebfbadd4fdc6bb24a4cc4
宁腾磊 <iscod@ningtengleideMacBook-Pro.local>
2016-12-26 23:06:46 +0800

    add first buy or first sell trading

--- application/controllers/cron/Trading.php
+++ application/controllers/cron/Trading.php
@@ -18,7 +18,8 @@ class Trading extends MY_Controller{
 
     private $max_order_num = 1;//最多同时在量的订单
     private $max_deal_num = 1;//未处理订单数大于该值不进行买入
-    private $min_balance_cny_num = 1300;//账户中保持最低人民币
+    private $min_balance_cny_num = 300;//账户中保持最低人民币
+    private $BuySellPro = 0.8;//先买策略与先卖策略比重buy/sell
 
 
      //是否下订单
@@ -29,13 +30,17 @@ class Trading extends MY_Controller{
         if ($type == "BTCCNY") $result = array('buy' => TRUE, 'sell' => TRUE);
 
         // //这里严重影响性能
-        // include_once APPPATH . '/third_party/btcchina-api/BTCChinaLibrary.php';
-        // $btcAPI = new BTCChinaAPI();
-        // // 获取我账户的信息
-        // $ret = $btcAPI->getAccountInfo();
+        include_once APPPATH . '/third_party/btcchina-api/BTCChinaLibrary.php';
+        $btcAPI = new BTCChinaAPI();
+        // 获取我账户的信息
+        $ret = $btcAPI->getAccountInfo();
 
-        // // 保持最低人民币
-        // if ($ret->balance->cny->amount < $this->min_balance_cny_num) $result['buy'] = FALSE;
+        // 保持最低人民币 通过客户端远程控制是否交易下一个较大的单使用户少于最低人民币
+        if ($ret->balance->cny->amount < $this->min_balance_cny_num) {
+            $result['buy'] = FALSE;
+            $result['sell'] = FALSE;
+        }
+
         return $result;
     }
 
@@ -129,15 +134,9 @@ class Trading extends MY_Controller{
         echo "done\n";
     }
 
-
-
     public function buybtc(){
         $is_price_order = $this->IsPlaceOrder('BTCCNY');
         if(!$is_price_order['buy']) die("done\n");
-
-        //当前开放的订单
-        $openOrders = $this->openOrders('BTCCNY');
-        if (count($openOrders['bid']) >= $this->max_order_num) die("order nums is max\n");
 
         include_once APPPATH . '/third_party/btcchina-api/BTCChinaLibrary.php';
         $btcAPI = new BTCChinaAPI();
@@ -150,25 +149,55 @@ class Trading extends MY_Controller{
         //已经买入的订单
         $redis = Rcache::init();
 
-        //当前开放的订单
-        if ($redis->sCard('Trading:Btc:OrderIds') > 0) die("order nums is max\n");
-
-        //未处理的订单数量
-        if($redis->lSize('trans:buy:btc') + count($redis->hGetAll('trans:sell:btc')) > $this->max_deal_num) die("waiting deal order num is max\n");
-
-        $amount = 0.06 * rand(80, 120) /100;
-        $amount = round($amount, 4);
-
         $orderId = 0;
-        $orderId = $btcAPI->placeOrder($price['buy'], $amount, 'BTCCNY');
+        //先买策略
+        if ($redis->sCard('Trading:Btc:OrderIds') < 1 && $redis->lSize('trans:buy:btc') + count($redis->hGetAll('trans:sell:btc')) < $this->max_deal_num) {
+            $amount = 0.06 * rand(80, 120) /100;
+            $amount = round($amount * $this->BuySellPro, 4);
 
-        if (!is_int($orderId) || $orderId < 1) die("error: " . var_dump($orderId));
+            $orderId = $btcAPI->placeOrder($price['buy'], $amount, 'BTCCNY');
 
-        $redis->sAdd('Trading:Btc:OrderIds', $orderId);
+            if (is_int($orderId) && $orderId > 1) {
+                $redis->sAdd('Trading:Btc:OrderIds', $orderId);
+                echo "First buy ok\n";   
+            }
+
+        }else{
+            echo "First buy done\n";
+        }
+
+        //先卖策略
+        if (count($redis->hGetAll('reverse:trans:buy:btc')) < $this->max_order_num) {
+            $tran = $redis->lPop('reverse:trans:sell:btc');
+
+            if ($tran) {
+                $tran = unserialize($tran);
+                if ($tran->avg_price < $price['buy']) {
+                    $redis->rPush('reverse:trans:sell:btc', serialize($tran));
+                }else{
+
+                    $amount = $tran->amount ? $tran->amount_original - $tran->amount : $tran->amount_original;//订单量
+
+                    //现在最高买的订单低于卖订单的价格4%的时候下单成交    
+                    if ($price['ask'] < ($tran->avg_price*0.96)) $price['buy'] = $price['ask'];
+
+                    $orderId = $btcAPI->placeOrder($price['buy'], round(($amount  * 100)/100 , 4), 'BTCCNY');
+
+                    //订单结束
+                    if (is_int($orderId) && $orderId > 0) {
+                        $redis->hSet('reverse:trans:buy:btc', $orderId, serialize($tran));
+                        echo "First sell ok\n";
+                    }else{
+                        $redis->rPush('reverse:trans:sell:btc', serialize($tran));
+                    }
+                }
+            }
+        }else{
+            echo "First sell done\n";
+        }
 
         echo "done\n"; 
     }
-
 
     public function sellbtc(){
         $is_price_order = $this->IsPlaceOrder('BTCCNY');
@@ -183,32 +212,50 @@ class Trading extends MY_Controller{
 
         $redis = Rcache::init();
 
-        if (count($redis->hGetAll('trans:sell:btc')) > $this->max_order_num) die("order nums is max\n");
+        $orderId = 0;
+        //先买策略
+        if (count($redis->hGetAll('trans:sell:btc')) < $this->max_order_num) {
+            $tran = $redis->lPop('trans:buy:btc');
+            if ($tran) {
+                $tran = unserialize($tran);
 
-        $tran = $redis->lPop('trans:buy:btc');
+                //超过交易时间那么就止损
+                if ($tran->avg_price > $price['sell'] && (time() - $tran->date) < 3600) {
+                    $redis->rPush('trans:buy:btc', serialize($tran));
+                }else{
+                    $amount = $tran->amount ? $tran->amount_original - $tran->amount : $tran->amount_original;//订单量
 
-        if (!$tran) die("no trans\n");
-    
-        $tran = unserialize($tran);
+                    //现在最高买的订单大于订单的价格4%的时候下单成交    
+                    if ($price['bid'] > ($tran->avg_price*1.01)) $price['sell'] = $price['bid'];
 
-        //超过交易时间那么就止损
-        if ($tran->avg_price > $price['sell'] && (time() - $tran->date) < 3600) {
-            $redis->rPush('trans:buy:btc', serialize($tran));
-            die("price is small\n");
+                    $orderId = $btcAPI->placeOrder($price['sell'], -round(($amount  * 100)/100 , 4), 'BTCCNY');
+
+                    //订单结束
+                    if (is_int($orderId) && $orderId > 0) {
+                        $redis->hSet('trans:sell:btc', $orderId, serialize($tran));
+                        echo "First buy ok\n";
+                    }else{
+                        $redis->rPush('trans:buy:btc', serialize($tran));
+                    }
+                }
+            }
+        }else{
+            echo "First buy is done\n";
         }
 
-        $amount = $tran->amount ? $tran->amount_original - $tran->amount : $tran->amount_original;//订单量
+        //先卖策略
+        if ($redis->sCard('Reverse:Trading:Btc:OrderIds') < 1 && $redis->lSize('reverse:trans:sell:btc') + count($redis->hGetAll('reverse:trans:buy:btc')) < $this->max_deal_num) {
 
-        //现在最高买的订单大于订单的价格4%的时候下单成交    
-        if ($price['bid'] > ($tran->avg_price*1.01)) $price['sell'] = $price['bid'];
+            $amount = 0.06 * rand(80, 120) /100;
+            $amount = round($amount / $this->BuySellPro, 4);
 
-        $sell_id = $btcAPI->placeOrder($price['sell'], -round(($amount  * 100)/100 , 4), 'BTCCNY');
-
-        //订单结束
-        if (is_int($sell_id) && $sell_id > 0) {
-            $redis->hSet('trans:sell:btc', $sell_id, serialize($tran));
+            $orderId = $btcAPI->placeOrder($price['sell'], -$amount, 'BTCCNY');
+            if (is_int($orderId) && $orderId > 1) {
+                $redis->sAdd('Reverse:Trading:Btc:OrderIds', $orderId);
+                echo "First sell ok\n";    
+            }
         }else{
-            $redis->rPush('trans:buy:btc', serialize($tran));
+            echo "First sell is done\n";
         }
 
         echo "done\n";
@@ -295,66 +342,128 @@ class Trading extends MY_Controller{
 
             //买订单处理
             if ($value->type == "bid") {
-                //非脚本下单和处理过的订单
-                if (!$redis->sIsMember('Trading:Btc:OrderIds' , $value->id)) continue;
 
-                switch ($value->status) {
-                    //取消的订单
-                    case 'cancelled':
-                        if ($value->amount == 0 || $value->amount == $value->amount_original) continue;
-                        $redis->rPush('trans:buy:btc', serialize($value));
-                        break;
+                //先买策略
+                if ($redis->sIsMember('Trading:Btc:OrderIds' , $value->id)) {
 
-                    //成交的订单
-                    case 'closed':
-                        $redis->rPush('trans:buy:btc', serialize($value));
-                        break;
+                    switch ($value->status) {
+                        //取消的订单
+                        case 'cancelled':
+                            if ($value->amount == 0 || $value->amount == $value->amount_original) continue;
+                            $redis->rPush('trans:buy:btc', serialize($value));
+                            break;
 
-                    //开放的订单
-                    case 'open':
-                        if((time() - $value->date) > 90) $btcAPI->cancelOrder((int)$value->id, 'BTCCNY');
-                        break;
+                        //成交的订单
+                        case 'closed':
+                            $redis->rPush('trans:buy:btc', serialize($value));
+                            break;
 
-                    default:
-                        # code...
-                        break;
+                        //开放的订单
+                        case 'open':
+                            if((time() - $value->date) > 90) $btcAPI->cancelOrder((int)$value->id, 'BTCCNY');
+                            break;
+
+                        default:
+                            # code...
+                            break;
+                    }
+
+                    if ($value->status != "open") $redis->sRem('Trading:Btc:OrderIds', $value->id);
                 }
 
-                if ($value->status != "open") $redis->sRem('Trading:Btc:OrderIds', $value->id);
+                //先卖策略
+                $info = $redis->hGet('reverse:trans:buy:btc', $value->id);
+                if ($info) {
+                    $info = unserialize($info);
 
+                    switch ($value->status) {
+                        //取消的订单
+                        case 'cancelled':
+                            if($value->amount_original != $value->amount && $value->amount > 0) $info->amount += $value->amount_original - $value->amount;//交易过的数量
+
+                            if($info->amount < $value->amount_original) $redis->rPush('reverse:trans:sell:btc', serialize($info));
+                            $redis->hDel('reverse:trans:buy:btc', $value->id);
+                            break;
+
+                        //成交的订单
+                        case 'closed':
+                            $redis->hDel('reverse:trans:buy:btc', $value->id);
+                            //只用作统计
+                            // $redis->hSet('reverse:trans:buy:btc:all:deal', $info->id, serialize($info));
+                            break;
+
+                        //开放的订单
+                        case 'open':
+                            if((time() - $value->date) > 120) $btcAPI->cancelOrder((int)$value->id, 'BTCCNY');
+                            break;
+
+                        default:
+                            # code...
+                            break;
+                    }
+                }
             }
 
             //卖订单处理
             if ($value->type == 'ask') {
+                //先买策略
                 $info = $redis->hGet('trans:sell:btc', $value->id);
-                if (!$info) continue;
+                if ($info) {
+                    $info = unserialize($info);
 
-                $info = unserialize($info);
+                    switch ($value->status) {
+                        //取消的订单
+                        case 'cancelled':
+                            if($value->amount_original != $value->amount && $value->amount > 0) $info->amount += $value->amount_original - $value->amount;//交易过的数量
 
-                switch ($value->status) {
-                    //取消的订单
-                    case 'cancelled':
-                        if($value->amount_original != $value->amount && $value->amount > 0) $info->amount += $value->amount_original - $value->amount;//交易过的数量
+                            if($info->amount < $value->amount_original) $redis->rPush('trans:buy:btc', serialize($info));
+                            $redis->hDel('trans:sell:btc', $value->id);
+                            break;
 
-                        if($info->amount < $value->amount_original) $redis->rPush('trans:buy:btc', serialize($info));
-                        $redis->hDel('trans:sell:btc', $value->id);
-                        break;
+                        //成交的订单
+                        case 'closed':
+                            $redis->hDel('trans:sell:btc', $value->id);
+                            //只用作统计
+                            // $redis->hSet('trans:sell:btc:all:deal', $info->id, serialize($info));
+                            break;
 
-                    //成交的订单
-                    case 'closed':
-                        $redis->hDel('trans:sell:btc', $value->id);
-                        //只用作统计
-                        // $redis->hSet('trans:sell:btc:all:deal', $info->id, serialize($info));
-                        break;
+                        //开放的订单
+                        case 'open':
+                            if((time() - $value->date) > 120) $btcAPI->cancelOrder((int)$value->id, 'BTCCNY');
+                            break;
 
-                    //开放的订单
-                    case 'open':
-                        if((time() - $value->date) > 120) $btcAPI->cancelOrder((int)$value->id, 'BTCCNY');
-                        break;
+                        default:
+                            # code...
+                            break;
+                    }                    
+                }
 
-                    default:
-                        # code...
-                        break;
+                //先卖策略
+                if ($redis->sIsMember('Reverse:Trading:Btc:OrderIds' , $value->id)) {
+                    switch ($value->status) {
+
+                        //取消的订单
+                        case 'cancelled':
+                            if ($value->amount == 0 || $value->amount == $value->amount_original) continue;
+                            $redis->rPush('reverse:trans:sell:btc', serialize($value));
+                            break;
+
+                        //成交的订单
+                        case 'closed':
+                            $redis->rPush('reverse:trans:sell:btc', serialize($value));
+                            break;
+
+                        //开放的订单
+                        case 'open':
+                            if((time() - $value->date) > 90) $btcAPI->cancelOrder((int)$value->id, 'BTCCNY');
+                            break;
+
+                        default:
+                            # code...
+                            break;
+                    }
+
+                    if ($value->status != "open") $redis->sRem('Reverse:Trading:Btc:OrderIds', $value->id);
                 }
             }
         }
@@ -387,17 +496,23 @@ class Trading extends MY_Controller{
 
     //谨慎操作
     public function reverse(){
-        // die("Warning!!!!!! \nAre you sure?\nPlease amend file!\n");
+        die("Warning!!!!!! \nAre you sure?\nPlease amend file!\n");
         $redis = Rcache::init();
+
+        $redis->delete('Trading:Ltc:OrderIds');
+        $redis->delete('trans:buy:ltc');
+        $redis->delete('trans:sell:ltc');
+        $redis->delete('trans:sell:ltc:all:deal');
+
         $redis->delete('trans:buy:btc');
         $redis->delete('trans:sell:btc');
         $redis->delete('Trading:Btc:OrderIds');
         $redis->delete('trans:sell:btc:all:deal');
 
-        $redis->delete('trans:buy:ltc');
-        $redis->delete('trans:sell:ltc');
-        $redis->delete('Trading:Btc:OrderIds');
-        $redis->delete('trans:sell:ltc:all:deal');
+        $redis->delete('reverse:trans:buy:btc');
+        $redis->delete('reverse:trans:sell:btc');
+        $redis->delete('Reverse:Trading:Btc:OrderIds');
+        $redis->delete('reverse:trans:buy:btc:all:deal');
         echo "done\n";
     }
 
