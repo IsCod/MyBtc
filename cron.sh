3460ebba9061f64950d3dcaed73c0dca81afa0e9
宁腾磊 <iscod@ningtengleideMacBook-Pro.local>
2016-12-28 10:28:29 +0800

    Monitor the process(监控进程防止进程过多)

--- cron.sh
+++ cron.sh
@@ -1,26 +1,63 @@
 #!/bin/bash
 function save()
 {
-	/usr/local/php/bin/php /Users/iscod/Data/MyBtc/index.php cron trading orderManage > /dev/null 2>&1
+	ps -fe|grep orderManage |grep -v grep
+	if [[ $? -ne 0 ]];
+		then
+			echo "OrderManage is start..."
+			php /data/web/MyBtc/index.php cron trading orderManage > /dev/null 2>&1
+		else
+			echo "OrderManage is runing...."
+	fi	
 }
 
 function sell()
 {
-	/usr/local/php/bin/php /Users/iscod/Data/MyBtc/index.php cron trading sellbtc > /dev/null 2>&1
-	/usr/local/php/bin/php /Users/iscod/Data/MyBtc/index.php cron trading sellltc > /dev/null 2>&1
+	ps -fe|grep orderManage |grep -v grep
+	if [[ $? -ne 0 ]];
+		then
+			echo "SellBtc is start..."
+			php /data/web/MyBtc/index.php cron trading sellbtc > /dev/null 2>&1
+		else
+			echo "SellBtc is runing...."
+	fi
+
+	# ps -fe|grep orderManage |grep -v grep
+	# if [[ $? -ne 0 ]];
+	# 	then
+	#		echo "SellLtc is start..."
+	# 		php /data/web/MyBtc/index.php cron trading sellltc > /dev/null 2>&1
+	# 	else
+	# 		echo "SellLtc is runing...."
+	# fi
 }
 
 function buy()
 {
-	/usr/local/php/bin/php /Users/iscod/Data/MyBtc/index.php cron trading buybtc > /dev/null 2>&1
-	/usr/local/php/bin/php /Users/iscod/Data/MyBtc/index.php cron trading buyltc > /dev/null 2>&1
+	ps -fe|grep orderManage |grep -v grep
+	if [[ $? -ne 0 ]];
+		then
+			echo "BuyBtc is start..."
+			php /data/web/MyBtc/index.php cron trading buybtc > /dev/null 2>&1
+		else
+			echo "BuyBtc is runing...."
+	fi
+
+	ps -fe|grep orderManage |grep -v grep
+	if [[ $? -ne 0 ]];
+		then
+			echo "BuyLtc is start..."
+			php /data/web/MyBtc/index.php cron trading buyltc > /dev/null 2>&1
+		else
+			echo "BuyLtc is runing...."
+	fi
 }
 
 #每分钟内执行次数
-for((i = 0; i < 10;  i++))
+for((i = 0; i < 30;  i++))
 do
 	save
 	sell
 	buy
-	sleep 5s
+	sleep 2s
 done
